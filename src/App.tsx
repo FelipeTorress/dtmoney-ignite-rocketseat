67a7cf0d2792eb4d163918ee40150b1063accc3d
Felipe Torres <felipetoress@gmail.com>
2021-12-14 17:04:27 -0300

    Refatorando codigo do modal

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Header } from "./components/Header";
 import { GlobalStyle } from "./styles/global";
 import { useState } from 'react';
 import Modal from 'react-modal';
+import { NewTransactionModal } from "./components/newTransactionModal";
 
 Modal.setAppElement('#root')
 
@@ -21,13 +22,14 @@ export function App() {
   return (
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+
       <Dashboard />
-      <Modal 
-          isOpen={isNewTransactionModalOpen}
-          onRequestClose={handleCloseNewTransactionModal}
-      >
-          <h2>Cadastrar Transação</h2>
-      </Modal>
+
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+      
       <GlobalStyle />
     </>
   );
